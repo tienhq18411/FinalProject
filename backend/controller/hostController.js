4205dd8b67ff5a91766f6829e16273fd7f46f872
tienhq18411 <tienhqgch18411@fpt.edu.vn>
2022-12-08 22:41:29 +0700

    final

--- backend/controller/hostController.js
+++ backend/controller/hostController.js
@@ -2,21 +2,19 @@ const Post = require("../models/post");
 const account = require("../models/accounts");
 const Comment = require("../models/comment");
 const bcrypt = require("bcrypt");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const jwt = require("jsonwebtoken");
 module.exports = {
   indexHost: async function (req, res) {
     const token = req.cookies.token;
-    let post = {}
+    let post = {};
     const project = { _id: 0 };
-    const query = req.query
-    console.log(query)
-    const _query = {}
-    _query['user.id'] = jwt.verify(token, "mk").id
-    if(query.searchKey) {
-      _query.$or = [
-        { title: { $regex: query.searchKey } },
-      ]
+    const query = req.query;
+    console.log(query);
+    const _query = {};
+    _query["user.id"] = jwt.verify(token, "mk").id;
+    if (query.searchKey) {
+      _query.$or = [{ title: { $regex: query.searchKey } }];
     }
     let sort = {
       createDate: -1,
@@ -27,22 +25,21 @@ module.exports = {
       };
       delete query.sort;
     }
-    _query.status = { $nin: ['DELETE','INIT'] }
+    _query.status = { $nin: ["DELETE", "INIT"] };
     const page = query.page || 1;
     const pageSize = query.pageSize || 9;
     delete query.page;
     delete query.pageSize;
-    post = await Post
-      .find(_query, project)
+    post = await Post.find(_query, project)
       .sort(sort)
       .skip(page * pageSize - pageSize)
       .limit(pageSize);
-    const count = await Post.countDocuments(_query)
+    const count = await Post.countDocuments(_query);
     const totalPage = Math.floor((count + pageSize - 1) / pageSize);
     const pagination = {
       page: page,
-      pageCount: totalPage
-    }
+      pageCount: totalPage,
+    };
     res.render("host/viewPost", { post: post, pagination: pagination });
   },
   createPost: function (req, res) {
@@ -50,15 +47,15 @@ module.exports = {
   },
   postCreatepost: async function (req, res) {
     const arrayUrl = req.files?.map((item) => item.filename);
-    const id = uuidv4()
+    const id = uuidv4();
     const token = req.cookies.token;
-    const user = await account.findOne({id:jwt.verify(token, "mk").id});
+    const user = await account.findOne({ id: jwt.verify(token, "mk").id });
     // tu token lay ra thong tin user roi xu li tiep
     const newPost = await new Post({
       id: id,
       title: req.body.title,
-      size: Number(req.body.size),
-      price: Number(req.body.price),
+      size: req.body.size,
+      price: req.body.price,
       city: req.body.city,
       district: req.body.district,
       ward: req.body.ward,
@@ -68,7 +65,7 @@ module.exports = {
       bath: req.body.bath,
       img: arrayUrl,
       user: user,
-      description: req.body.description
+      description: req.body.description,
     });
     //console.log(newPost);
     await newPost.save();
@@ -76,27 +73,30 @@ module.exports = {
   },
   updatePost: async function (req, res) {
     const id = req.params.id;
-    const newPost = await Post.findOne({id: id});
+    const newPost = await Post.findOne({ id: id });
     res.render("host/updatePost", { newPost: newPost });
   },
   postUpdatePost: async function (req, res) {
     const arrayUrl = req.files?.map((item) => item.filename);
     const id = req.body.id;
 
-    await Post.updateOne({id: id}, {
-      title: req.body.title,
-      size: Number(req.body.size),
-      price: Number(req.body.price),
-      city: req.body.city,
-      district: req.body.district,
-      ward: req.body.ward,
-      addressDetail: req.body.addressDetail,
-      addressMap: req.body.addressMap,
-      bed: req.body.bed,
-      bath: req.body.bath,
-      img: arrayUrl,
-      description: req.body.description
-    });
+    await Post.updateOne(
+      { id: id },
+      {
+        title: req.body.title,
+        size: req.body.size,
+        price: req.body.price,
+        city: req.body.city,
+        district: req.body.district,
+        ward: req.body.ward,
+        addressDetail: req.body.addressDetail,
+        addressMap: req.body.addressMap,
+        bed: req.body.bed,
+        bath: req.body.bath,
+        img: arrayUrl,
+        description: req.body.description,
+      }
+    );
     res.redirect("/host");
   },
   deletePost: async function (req, res) {
@@ -105,11 +105,11 @@ module.exports = {
   },
 
   commentPost: async function (req, res) {
-    const postId = req.body.postId
-    const id = uuidv4()
+    const postId = req.body.postId;
+    const id = uuidv4();
     const token = req.cookies.token;
-    const user = await account.findOne({id:jwt.verify(token, "mk").id});
-    const post = await Post.findOne({id: postId})
+    const user = await account.findOne({ id: jwt.verify(token, "mk").id });
+    const post = await Post.findOne({ id: postId });
     const comment = await new Comment({
       comment: req.body.comment,
       user: user,
@@ -119,34 +119,37 @@ module.exports = {
     await comment.save();
     res.redirect(`/auth/detail/${postId}`);
   },
-  updateStatusPost: async function (req,res) {
-    const id = req.params.id
-    const status = req.query.action
-    await Post.updateOne({id: id}, {status: status})
+  updateStatusPost: async function (req, res) {
+    const id = req.params.id;
+    const status = req.query.action;
+    await Post.updateOne({ id: id }, { status: status });
     res.redirect(`/host`);
   },
   viewUserInfo: async function (req, res) {
     const token = req.cookies.token;
     let user = {};
-    if(token) {
-      user = await account.findOne({id:jwt.verify(token, "mk").id});
+    if (token) {
+      user = await account.findOne({ id: jwt.verify(token, "mk").id });
     }
-    res.render("host/accountInfo", { user: user })
+    res.render("host/accountInfo", { user: user });
   },
   changeUserInfo: async function (req, res) {
-    let hashed = ''
-    if(req.body.password) {
+    let hashed = "";
+    if (req.body.password) {
       const salt = await bcrypt.genSalt(10);
       hashed = await bcrypt.hash(req.body.password, salt);
     }
-    user = await account.findOne({id:req.body.id});
-    console.log(req.body)
-    await account.updateOne({id: user.id},{
-      name: req.body.name,
-      username: req.body.username,
-      password: hashed || user.password,
-      phone: req.body.phone,
-    })
+    user = await account.findOne({ id: req.body.id });
+    console.log(req.body);
+    await account.updateOne(
+      { id: user.id },
+      {
+        name: req.body.name,
+        username: req.body.username,
+        password: hashed || user.password,
+        phone: req.body.phone,
+      }
+    );
     res.redirect("/host/viewUserInfo");
-  }
+  },
 };
--- backend/models/post.js
+++ backend/models/post.js
@@ -3,30 +3,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const account = mongoose.Schema({
-  id: { type: String }, 
-  name: { type: String }, 
+  id: { type: String },
+  name: { type: String },
   username: { type: String },
-  phone: {type: String},
-  createDate: {type: Date, default: () => Date.now()},
-  isActive: {type: Boolean}
+  phone: { type: String },
+  createDate: { type: Date, default: () => Date.now() },
+  isActive: { type: Boolean },
 });
 const post = mongoose.Schema({
   id: { type: String },
   title: { type: String },
   size: { type: Number },
   price: { type: Number },
-  city: { type: String }, 
-  district: { type: String }, 
+  city: { type: String },
+  district: { type: String },
   ward: { type: String },
-  addressDetail: {type: String},
+  addressDetail: { type: String },
   addressMap: { type: String },
   bed: { type: String },
   bath: { type: String },
-  description: {type: String},
-  status: { type: String, default: 'PENDING' },
-  img:[String],
-  user: { type: account},
-  createDate: {type: Date, default: () => Date.now()}
+  description: { type: String },
+  status: { type: String, default: "PENDING" },
+  img: [String],
+  user: { type: account },
+  createDate: { type: Date, default: () => Date.now() },
 });
 
 module.exports = mongoose.model("post ", post);
